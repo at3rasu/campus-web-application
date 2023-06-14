d746accb2d47b5038e01dfe1815b3e47fef29690
Artur Imankulov <arturimankulov906@gmail.com>
2023-06-14 17:50:03 +0500

    fix vac

--- campus-server/dist/vacancies/vacancies.service.d.ts
+++ campus-server/dist/vacancies/vacancies.service.d.ts
@@ -1,15 +1,13 @@
 import { CreateVacancyDto } from './dto/create-vacancy.dto';
 import { Vacancy } from './vacancies.model';
 import { JwtService } from '@nestjs/jwt';
-import { UploadFilesService } from 'src/upload-files/upload-files.service';
 import { UsersCompanyService } from 'src/users-company/users-company.service';
 export declare class VacanciesService {
     private vacancyRepository;
     private jwtService;
-    private uploadFilesService;
     private userCompanyService;
-    constructor(vacancyRepository: typeof Vacancy, jwtService: JwtService, uploadFilesService: UploadFilesService, userCompanyService: UsersCompanyService);
-    createVacancy(vacancyDto: CreateVacancyDto, image: any, req: any): Promise<{
+    constructor(vacancyRepository: typeof Vacancy, jwtService: JwtService, userCompanyService: UsersCompanyService);
+    createVacancy(vacancyDto: CreateVacancyDto, req: any): Promise<{
         token: string;
     }>;
     private generateToken;
